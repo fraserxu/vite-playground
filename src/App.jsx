242a24a1afb018d5ab366c0d2b39943cbe6957dc
Fraser Xu <xvfeng123@gmail.com>
2020-05-24 11:12:05 +1000

    move dropdown into a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,8 @@
 import React, { useState, useCallback } from 'react'
-import { useSpring, animated } from 'react-spring'
+
+import Dropdwon from './dropdown'
 
 function App() {
-  const [isOpen, setOpen] = useState(null)
-
-  const handleClick = useCallback(() => {
-    setOpen(!isOpen)
-  }, [isOpen, setOpen])
-
-  const opacity = isOpen === null ? 0 : isOpen ? 0 : 1
-
-  const props = useSpring({
-    opacity: isOpen ? 1 : 0,
-    from: { opacity },
-  })
   return (
     <div className="max-w-2xl mx-auto">
       <header className="flex justify-center py-16 text-2xl">
@@ -21,8 +10,9 @@ function App() {
       </header>
 
       <main>
-        <ul className="flex flex-col list-disc">
-          <li className="">
+        <h2 className="py-4 px-0 text-lg">Stacks:</h2>
+        <ul className="flex flex-col list-disc pl-4">
+          <li>
             <a
               className="hover:underline"
               href="https://github.com/vitejs/vite"
@@ -42,90 +32,12 @@ function App() {
           </li>
         </ul>
 
-        <div className="flex justify-center my-8">
-          <div className="relative inline-block text-left">
-            <div>
-              <span className="rounded-md shadow-sm">
-                <button
-                  type="button"
-                  onClick={handleClick}
-                  className="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition ease-in-out duration-150"
-                >
-                  Options
-                  <svg
-                    className="-mr-1 ml-2 h-5 w-5"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </button>
-              </span>
-            </div>
-            <animated.div style={props}>
-              <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg">
-                <div className="rounded-md bg-white shadow-xs">
-                  <div className="py-1">
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
-                    >
-                      Edit
-                    </a>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
-                    >
-                      Duplicate
-                    </a>
-                  </div>
-                  <div className="border-t border-gray-100"></div>
-                  <div className="py-1">
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
-                    >
-                      Archive
-                    </a>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
-                    >
-                      Move
-                    </a>
-                  </div>
-                  <div className="border-t border-gray-100"></div>
-                  <div className="py-1">
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
-                    >
-                      Share
-                    </a>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
-                    >
-                      Add to favorites
-                    </a>
-                  </div>
-                  <div className="border-t border-gray-100"></div>
-                  <div className="py-1">
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
-                    >
-                      Delete
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </animated.div>
-          </div>
+        <div className="mx-auto my-8">
+          <h2 className="py-4 px-0 text-lg">
+            Animation with <span className="font-bold">react-spring</span>{' '}
+            example:
+          </h2>
+          <Dropdwon />
         </div>
       </main>
     </div>
